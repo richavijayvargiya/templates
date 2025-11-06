a86b4b4097adb39f4f3c16dce2006804ae9ce895
richa.vijayvargiya <richa.vijayvargiya@cognizant.com>
2025-11-06 16:58:14 +0000

    install command is added

--- nuxt/idx-template.nix
+++ nuxt/idx-template.nix
@@ -1,54 +1,52 @@
-{ pkgs, packageManager, ... }: {
-     channel = "stable-25.05";
-    packages = [
-      pkgs.nodejs_20
-      pkgs.git
-    ];
+      { pkgs, packageManager, ... }: {
+           channel = "stable-25.05";
+          packages = [
+            pkgs.nodejs_20
+            pkgs.git
+          ];
 
-    # Available options as of 1/17/2024
-    # https://github.com/nuxt/cli/blob/f113a083f000d19c9ae7f35ae2534ac5c0dba77b/src/commands/init.ts
-    # npx nuxi@latest init nuxt-idx --package-manager bun --install true --git-init true --force true
+          # Available options as of 1/17/2024
+          # https://github.com/nuxt/cli/blob/f113a083f000d19c9ae7f35ae2534ac5c0dba77b/src/commands/init.ts
+          # npx nuxi@latest init nuxt-idx --package-manager bun --install true --git-init true --force true
 
-    # To test this configuration:
-    # /nix/store/mvr5wczap3ga80iq548n2griy8kx9ksx-idx-template/bin/idx-template ~/Monospace/workspace/nix_templates/public/nuxt --output-dir ~ --workspace-name foo -a '{"packageManager": "bun"}'
+          # To test this configuration:
+          # /nix/store/mvr5wczap3ga80iq548n2griy8kx9ksx-idx-template/bin/idx-template ~/Monospace/workspace/nix_templates/public/nuxt --output-dir ~ --workspace-name foo -a '{"packageManager": "bun"}'
 
-    bootstrap = ''
-      npx --prefer-offline -y @nuxt/cli start "$WS_NAME" blank --type=nuxt --no-deps --no-git --no-link --no-
-      npx nuxi@latest -y init "$out" \
-        --package-manager ${packageManager} \
-        --no-install \
-        --git-init
+          bootstrap = ''
+            npx nuxi@latest -y init "$out" \
+              --package-manager ${packageManager} \
+              --no-install \
+              --git-init
 
-      mkdir "$out"/.idx
-      cp ${./dev.nix} "$out"/.idx/dev.nix
-      chmod -R +w "$out"
+            mkdir "$out"/.idx
+            cp ${./dev.nix} "$out"/.idx/dev.nix
+            chmod -R +w "$out"
 
-      sed -i "s/PACKAGE_MANAGER/${packageManager}/g" "$out"/.idx/dev.nix
+            sed -i "s/PACKAGE_MANAGER/${packageManager}/g" "$out"/.idx/dev.nix
+            sed -i "s/PM_COMMAND/${{
+              "npm": "npm install",
+              "pnpm": "pnpm install",
+              "bun": "bun install",
+              "yarn": "yarn install"
+            }[packageManager]}/g" "$out"/.idx/dev.nix
 
-      sed -i "s/PM_COMMAND/${
-        if packageManager == "npm" then
-          "npm ci --no-audit --prefer-offline --no-progress --timing"
-        else
-          "${packageManager} install"        
-      }/g" "$out"/.idx/dev.nix
+            sed -i "s/PM_NIX_PACKAGE/${
+              if packageManager == "npm" then
+                ""
+              else if packageManager == "pnpm" then
+                "pkgs.nodePackages.pnpm"
+              else if packageManager == "bun" then
+                "pkgs.bun"
+              else
+                "pkgs.yarn"
+            }/g" "$out"/.idx/dev.nix
 
-      sed -i "s/PM_NIX_PACKAGE/${
-        if packageManager == "npm" then
-          ""
-        else if packageManager == "pnpm" then
-          "pkgs.nodePackages.pnpm"
-        else if packageManager == "bun" then
-          "pkgs.bun"
-        else
-          "pkgs.yarn"
-      }/g" "$out"/.idx/dev.nix
+            mkdir -p "$out/.idx"
+            chmod -R u+w "$out"
+            cp -rf ${./.idx/airules.md} "$out"/.idx/airules.md"
+            cp -rf "$out/.idx/airules.md" "$out/GEMINI.md"
+            chmod -R u+w "$out"
 
-      mkdir -p "$out/.idx"
-      chmod -R u+w "$out"
-      cp -rf ${./.idx/airules.md} "$out/.idx/airules.md"
-      cp -rf "$out/.idx/airules.md" "$out/GEMINI.md"
-      chmod -R u+w "$out"
-
-      ${if packageManager == "npm" then "( cd \$out && npm i --package-lock-only --ignore-scripts )" else ""}
-    '';
-}
+            ${if packageManager == "npm" then "( cd \$out && npm i --package-lock-only --ignore-scripts )" else ""}
+          '';
+      }
