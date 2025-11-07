38f8f858a39ac5e34a12bf051b9c5de58e29c6b4
richa.vijayvargiya <richa.vijayvargiya@cognizant.com>
2025-11-07 19:40:53 +0000

    reverted

--- nuxt/idx-template.nix
+++ nuxt/idx-template.nix
@@ -13,9 +13,11 @@
     # /nix/store/mvr5wczap3ga80iq548n2griy8kx9ksx-idx-template/bin/idx-template ~/Monospace/workspace/nix_templates/public/nuxt --output-dir ~ --workspace-name foo -a '{"packageManager": "bun"}'
 
     bootstrap = ''
+      echo "No" | \
       npx -y nuxi@latest init "$out" \
         --package-manager ${packageManager} \
         --no-install \
+        --no-git \
         --force
 
       mkdir "$out"/.idx
