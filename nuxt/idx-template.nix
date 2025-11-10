53cb528dc122647507ab242e46fa368fd2161318
richa.vijayvargiya <richa.vijayvargiya@cognizant.com>
2025-11-10 06:59:43 +0000

    revert

--- nuxt/idx-template.nix
+++ nuxt/idx-template.nix
@@ -1,77 +1,55 @@
-{ pkgs, ... }:
+{ pkgs, packageManager, ... }: {
 
-let
-  # The Node.js version to use. This is read from the `.nvmrc` file
-  # in the workspace root. If the file doesn't exist, it defaults to "20".
-  nodeVersion = pkgs.lib.strings.fileContents ./.nvmrc or "20";
-  # The corresponding Nix package for the Node.js version.
-  nodejs = pkgs."nodejs_$\{nodeVersion\}_x";
+    packages = [
+      pkgs.nodejs_24    
+      pkgs.nodePackages.npm-check-updates
+    ];
 
-  # The package manager to use. This is configured in the bootstrap script.
-  packageManager = "PACKAGE_MANAGER";
-  # The command to install dependencies.
-  installCommand = "PM_COMMAND";
-  # The command to run the development server.
-  devCommand = "DEV_COMMAND";
-in
-{
-  # metadata for the template
-  name = "Nuxt";
-  description = "Create a Nuxt application.";
-  author = "Google";
-  tags = ["web", "framework", "ssr", "ssg", "typescript", "javascript"];
-  icon = ./icon.png;
+    # Available options as of 1/17/2024
+    # https://github.com/nuxt/cli/blob/f113a083f000d19c9ae7f35ae2534ac5c0dba77b/src/commands/init.ts
+    # npx nuxi@latest init nuxt-idx --package-manager bun --install true --git-init true --force true
 
-  # a nix expression for the development environment
-  environment.dev = ./dev.nix;
+    # To test this configuration:
+    # /nix/store/mvr5wczap3ga80iq548n2griy8kx9ksx-idx-template/bin/idx-template ~/Monospace/workspace/nix_templates/public/nuxt --output-dir ~ --workspace-name foo -a '{"packageManager": "bun"}'
 
-  # what to do to bootstrap the project
-  bootstrap = {
-    args = {
-      packageManager = {
-        description = "The package manager to use for the project.";
-        oneOf = ["npm", "yarn", "pnpm", "bun"];
-        default = "npm";
-      };
-    };
-    script = ''
-      # create a .nvmrc file with a default version
-      echo "20" > "$out"/.nvmrc
-
-      # initialize the nuxt project
+    bootstrap = ''
       echo "No" | \
       npx -y nuxi@latest init "$out" \
-        --package-manager $\{packageManager\} \
+        --package-manager ${packageManager} \
         --no-install \
         --no-git \
         --force
 
-      # copy over the .idx configuration
       mkdir "$out"/.idx
-      cp $\{./dev.nix\} "$out"/.idx/dev.nix
+      cp ${./dev.nix} "$out"/.idx/dev.nix
       chmod -R +w "$out"
 
-      #
-      # Below, we are dynamically configuring the dev.nix based on the chosen package manager.
-      #
-      PM_COMMAND="echo 'install command is not set'"
-      if [ "$\{packageManager\}" = "npm" ]; then
-        PM_COMMAND="npm install"
-        DEV_COMMAND="npm run dev"
-      elif [ "$\{packageManager\}" = "yarn" ]; then
-        PM_COMMAND="yarn"
-        DEV_COMMAND="yarn dev"
-      elif [ "$\{packageManager\}" = "pnpm" ]; then
-        PM_COMMAND="pnpm install"
-        DEV_COMMAND="pnpm run dev"
-      elif [ "$\{packageManager\}" = "bun" ]; then
-        PM_COMMAND="bun install"
-        DEV_COMMAND="bun run dev"
-      fi
+      sed -i "s/PACKAGE_MANAGER/${packageManager}/g" "$out"/.idx/dev.nix
 
-      sed -i "s/PACKAGE_MANAGER/$\{packageManager\}/g" "$out"/.idx/dev.nix
-      sed -i "s|PM_COMMAND|''$\{PM_COMMAND\}''|g" "$out"/.idx/dev.nix
-      sed -i "s|DEV_COMMAND|''$\{DEV_COMMAND\}''|g" "$out"/.idx/dev.nix
+      sed -i "s/PM_COMMAND/${
+        if packageManager == "npm" then
+          "npm ci --no-audit --prefer-offline --no-progress --timing"
+        else
+          "${packageManager} install"        
+      }/g" "$out"/.idx/dev.nix
+
+      sed -i "s/PM_NIX_PACKAGE/${
+        if packageManager == "npm" then
+          ""
+        else if packageManager == "pnpm" then
+          "pkgs.nodePackages.pnpm"
+        else if packageManager == "bun" then
+          "pkgs.bun"
+        else
+          "pkgs.yarn"
+      }/g" "$out"/.idx/dev.nix
+
+      mkdir -p "$out/.idx"
+      chmod -R u+w "$out"
+      cp -rf ${./.idx/airules.md} "$out/.idx/airules.md"
+      cp -rf "$out/.idx/airules.md" "$out/GEMINI.md"
+      chmod -R u+w "$out"
+
+      ${if packageManager == "npm" then "( cd \$out && npm i --package-lock-only --ignore-scripts )" else ""}
     '';
-  };
 }
