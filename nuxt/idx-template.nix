108bacfa63b4318bc3db9716753070f62db5bbe1
richa.vijayvargiya <richa.vijayvargiya@cognizant.com>
2025-11-11 15:04:17 +0000

    -y moved

--- nuxt/idx-template.nix
+++ nuxt/idx-template.nix
@@ -13,7 +13,7 @@
     # /nix/store/mvr5wczap3ga80iq548n2griy8kx9ksx-idx-template/bin/idx-template ~/Monospace/workspace/nix_templates/public/nuxt --output-dir ~ --workspace-name foo -a '{"packageManager": "bun"}'
 
     bootstrap = ''
-       npx -y nuxi@latest init "$out" \
+       npx nuxi@latest -y init "$out" \
         --package-manager ${packageManager} \
         --no-install \
         --no-git \
