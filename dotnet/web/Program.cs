1d0956d4f8ab5d0a34aa73ca4847e4aecd46adb0
richa.vijayvargiya <richa.vijayvargiya@cognizant.com>
2025-11-13 10:04:31 +0000

    update program.cs

--- dotnet/web/Program.cs
+++ dotnet/web/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "3000";
+var port = Environment.GetEnvironmentVariable("PORT") ?? "9002";
 var url = $"http://0.0.0.0:{port}";
 var target = Environment.GetEnvironmentVariable("TARGET") ?? "World";
 
